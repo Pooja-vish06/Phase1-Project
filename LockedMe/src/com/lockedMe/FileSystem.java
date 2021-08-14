ede12f86985e190cfb309c8a8dcffdeb02710215
Pooja-vish6 <pooja.vishwakarma@northgateps.com>
2021-08-14 15:01:52 +0530

    This is the final project with documentation.

--- LockedMe/src/com/lockedMe/FileSystem.java
+++ LockedMe/src/com/lockedMe/FileSystem.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 public class FileSystem {
 
+	// variable declaration
 	public static final String FOLDERPATH = "/home/pooja/Documents/Simplilern/Phase1-Project/Files";
 
 	/**
 	 * This method provides the name of all files present in the defined folder.
 	 * 
+	 * @return List of files
 	 * @String
 	 */
 	public static List<String> retrieveAllFiles() {
@@ -30,7 +32,8 @@ public class FileSystem {
 	}
 
 	/**
-	 * This method creates a new file and adds the content provided by the user
+	 * This method creates a new file and adds the content provided by the user,
+	 * returns true if file is added.
 	 * 
 	 * @param fileName
 	 * @param fileContent
@@ -51,6 +54,39 @@ public class FileSystem {
 
 			return false;
 		}
+
+	}
+
+	/**
+	 * This method deletes the file specified by user, returns true if deleted.
+	 * 
+	 * @param fileName
+	 * @return boolean
+	 */
+	public static boolean deleteFile(String fileName) {
+
+		File file = new File(FOLDERPATH + "/" + fileName);
+		if (file.delete())
+			return true;
+		else
+			return false;
+
+	}
+
+	/**
+	 * This method searches the user specified file in the locker folder, returns
+	 * true if found.
+	 * 
+	 * @param fileName
+	 * @return boolean
+	 */
+	public static boolean searchFile(String fileName) {
+
+		File file = new File(FOLDERPATH + '/' + fileName);
+		if (file.exists())
+			return true;
+		else
+			return false;
 
 	}
 
--- LockedMe/src/com/lockedMe/FilesLocker.java
+++ LockedMe/src/com/lockedMe/FilesLocker.java
@@ -8,67 +8,104 @@ import java.util.Scanner;
 public class FilesLocker {
 
 	public static void main(String[] args) {
-		
-		//Variable declaration
+
+		// Variable declaration
 		int option = 1;
-		
-		//loop till user exits the system
+
+		// loop till user exits the system
 		do {
+			welcomePage();
 			int choice = displayMenu();
-			switch(choice) {
-			case 1 : getFiles();
-					break;
-			case 2 : addNewFile();
-					break;
-			case 0 : 
+			switch (choice) {
+			case 1:
+				getFiles();
+				break;
+			case 2:
+				addNewFile();
+				break;
+			case 3:
+				deleteFile();
+				break;
+			case 4:
+				searchFile();
+				break;
+			case 5:
 				option = 0;
 				System.out.println("Thank you for using our services.");
+				System.out.println("Developed by - Pooja Vishwakarma.");
 				System.exit(0);
 				break;
-			default : 
-				System.out.println("Invalid input. ");
+			default:
+				System.out.println("Invalid input. Please enter valid option. ");
 			}
-			
-		}
-		while(option > 0);
 
-		
+		} while (option > 0);
 
 	}
-	public static int  displayMenu() {
-		System.out.println("1. Display all Files.");
-		System.out.println("2. Add a new File.");
-		System.out.println("Enter your choice : ");
+
+	// =====WELCOME PAGE=====
+	public static void welcomePage() {
+		System.out.println("=================================================");
+		System.out.println("$\t\tLOCKER PVT LTD.\t\t\t$");
+		System.out.println("\tDeveloped by - Pooja Vishwakarma");
+		System.out.println("=================================================");
+
+	}
+
+	// ======DISPLAY MENU======
+	public static int displayMenu() {
+
+		//
 		Scanner sc = new Scanner(System.in);
-		int choice = sc.nextInt();
-		if (choice == 0)
-			choice = 0;
-		return choice;
+
+		System.out.println("\t1. Display all Files.");
+		System.out.println("\t2. Add a new File.");
+		System.out.println("\t3. Delete a File.");
+		System.out.println("\t4. Search a File.");
+		System.out.println("\t5. Exit.");
+		System.out.println("=================================================");
+		System.out.println("Enter your choice : ");
+
+		// take choice from user.
+
+		try {
+			int choice = Integer.parseInt(sc.nextLine());
+			return choice;
+		} catch (NumberFormatException e) {
+			System.out.println("You've entered non-integer number. Please enter valid option from below.");
+			System.out.println("=================================================");
+			return displayMenu();
+		}
+
 	}
-	// ==========GET ALL FILES===========
+
+	// ======GET ALL FILES======
 	public static void getFiles() {
 		int count = 0;
 		List<String> files = FileSystem.retrieveAllFiles();
 		count = files.size();
 		System.out.println("Total " + count + " files are present.");
+		System.out.println("Files are : ");
 		Collections.sort(files);
 		for (String s : files) {
 			System.out.println(s);
 		}
 	}
 
-	// =========Add A FILE===============
-	// This method takes input from user and calls addFile function to create a new
-	// file
+	// ======Add A FILE======
 	public static void addNewFile() {
 		Scanner sc = new Scanner(System.in);
 		List<String> fileContent = new ArrayList<String>();
+
+		// User will input the file name
 		System.out.println("Enter your file name :");
 		String fileName = sc.nextLine();
-		System.out.println(fileName);
+
+		// User will enter number of lines here
 		System.out.println("Enter number of lines you want to add :");
 		int linesCount = Integer.parseInt(sc.nextLine());
 
+		// Loop through number of lines
 		for (int i = 1; i <= linesCount; i++) {
 			System.out.println("Enter line " + i + ":");
 			String lines = sc.nextLine();
@@ -78,8 +115,38 @@ public class FilesLocker {
 		boolean fileAdded = FileSystem.addFile(fileName, fileContent);
 		if (fileAdded)
 			System.out.println("File saved successfully.");
-		else
+		else {
 			System.out.println("File could not be saved. Try again later.");
+			System.out.println("if probelm persists, contact admin admin@lockerme.com.");
+		}
+
+	}
+
+	// ======DELETE A FILE======
+	public static void deleteFile() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the file name to be deleted :");
+		String fileName = sc.nextLine();
+		boolean isDeleted = FileSystem.deleteFile(fileName);
+		if (isDeleted) {
+			System.out.println(fileName + " deleted successfully.");
+		} else {
+			System.out.println(fileName + " couldn't be deleted. Try again later.");
+			System.out.println("if probelm persists, contact admin admin@lockerme.com.");
+		}
+
+	}
+
+	// ======SEARCH A FILE======
+	public static void searchFile() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter file name to be searched");
+		String fileName = sc.nextLine();
+		boolean isFound = FileSystem.searchFile(fileName);
+		if (isFound)
+			System.out.println(fileName + " is present in the locker.");
+		else
+			System.out.println(fileName + " is not present in the locker.");
 
 	}
 
